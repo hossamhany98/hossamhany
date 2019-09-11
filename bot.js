a3baca58a3f7522a52600e8f838a6e6b1d30adeb
hossamhany98 <55183016+hossamhany98@users.noreply.github.com>
2019-09-11 01:52:39 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("620406415995043850")
+setInterval(function() {
+channel.send(`شوف فيك يا زم تتخويت علي انا الاسطورة بطلع في الصورة`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
